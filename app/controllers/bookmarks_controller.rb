171aa4ffa8d4885e776338d71dc7e9e3957df6f8
raymond ong <rayjosong@gmail.com>
2022-05-27 15:44:47 +0800

    cleared rake

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,38 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new()
+
+    # redirect_to list_path(@list)
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.movie = find_movie
+    @bookmark.list = @list
+    @bookmark.save
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+
+  def find_movie
+    @movie = Movie.find(params[:bookmark][:movie_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [ :index, :new, :show, :create ] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
+
+
+# A user can see all the lists
+# GET "lists"
+
+# A user can see the details of a given list and its name
+# GET "lists/42"
+
+# A user can create a new list
+# GET "lists/new"
+# POST "lists"
